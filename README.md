05212bd1a55ff766ceef44fe56b6982c79aefb46
karupanerura <karupa@cpan.org>
2013-12-24 01:02:58 +0900

    initialize commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Time::Strptime - It's new $module
+
+# SYNOPSIS
+
+    use Time::Strptime;
+
+# DESCRIPTION
+
+Time::Strptime is ...
+
+# LICENSE
+
+Copyright (C) karupanerura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+karupanerura <karupa@cpan.org>
---
+++ lib/Time/Strptime.pm
@@ -0,0 +1,39 @@
+package Time::Strptime;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Time::Strptime - It's new $module
+
+=head1 SYNOPSIS
+
+    use Time::Strptime;
+
+=head1 DESCRIPTION
+
+Time::Strptime is ...
+
+=head1 LICENSE
+
+Copyright (C) karupanerura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+karupanerura E<lt>karupa@cpan.orgE<gt>
+
+=cut
+
