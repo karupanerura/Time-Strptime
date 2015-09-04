de4914dd64f6e08857449e87c5a2ecefb567f35c
karupanerura <karupa@cpan.org>
2015-09-04 11:48:01 +0200

    Use Time::Moment on repl.pl

--- author/repl.pl
+++ author/repl.pl
@@ -4,13 +4,20 @@ use utf8;
 use feature qw/say/;
 
 use Time::Strptime::Format;
-use Time::Piece;
+use Time::Moment;
+use Getopt::Long ':config' => qw/posix_default no_ignore_case bundling auto_help/;
 
-my $format = Time::Strptime::Format->new($ARGV[0]);
+GetOptions(
+    \my %opt, qw/
+    locale=s
+/) or die "Usage: $0 [--locale=C] format";
+
+my $format = Time::Strptime::Format->new($ARGV[0], \%opt);
 
 while (1) {
     print '> ';
     chomp(my $line = <STDIN>);
+    utf8::decode($line);
     my ($epoch, $offset) = eval { $format->parse($line) };
     if ($@) {
         print "ERROR: $@";
@@ -18,7 +25,7 @@ while (1) {
     }
     print "epoch     = $epoch\n";
     print "offset    = $offset\n";
-    print "localtime = @{[ scalar localtime $epoch ]}\n";
-    print "gmtime    = @{[ scalar gmtime $epoch ]}\n";
+    print "localtime = @{[ Time::Moment->from_epoch($epoch)->with_offset($offset / 60)->to_string ]}\n";
+    print "gmtime    = @{[ Time::Moment->from_epoch($epoch)->to_string ]}\n";
 }
 __END__
