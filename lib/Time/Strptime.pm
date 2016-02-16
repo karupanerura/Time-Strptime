1a8bbcb1bf2a3ce4ef6f31ac370575c869c54b70
karupanerura <karupa@cpan.org>
2016-02-16 10:24:15 +0900

    Checking in changes prior to tagging of version 0.07.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ac93695..2fd0290 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Time-Strptime
    
     {{$NEXT}}
    
    +0.07 2016-02-16T01:23:55Z
    +
    +    - fixed dependency
    +
     0.06 2016-02-15T06:56:51Z
    
         - allow to omit the day of month

--- lib/Time/Strptime.pm
+++ lib/Time/Strptime.pm
@@ -3,7 +3,7 @@ use 5.008005;
 use strict;
 use warnings;
 
-our $VERSION = "0.06";
+our $VERSION = "0.07";
 
 use parent qw/Exporter/;
 our @EXPORT_OK = qw/strptime/;
--- lib/Time/Strptime/Format.pm
+++ lib/Time/Strptime/Format.pm
@@ -16,7 +16,7 @@ use Time::Strptime::TimeZone;
 
 use constant DEBUG => exists $ENV{PERL_TIME_STRPTIME_DEBUG} && $ENV{PERL_TIME_STRPTIME_DEBUG};
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 our %DEFAULT_HANDLER = (
     '%' => [char          => '%' ],
